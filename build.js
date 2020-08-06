9c31f4eeacdf6e5bf9dd573171b61efda061e2b5
Mike Farrow <contact@mikefarrow.co.uk>
2020-08-06 11:17:44 +0100

    adding trim before slugify

--- build.js
+++ build.js
@@ -14,6 +14,8 @@ function slugify(str) {
   return str
     .toString()
     .toLowerCase()
+    .replace(/^-+/, '') // Trim - from start of text
+    .replace(/-+$/, ''); // Trim - from end of text
     .replace(/\s+/g, '-') // Replace spaces with -
     .replace(/[^\w\-]+/g, '') // Remove all non-word chars
     .replace(/\-\-+/g, '-') // Replace multiple - with single -
