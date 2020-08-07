344335519174796dbae34cb3149d1794fe515695
Mike Farrow <contact@mikefarrow.co.uk>
2020-08-07 01:07:51 +0100

    adding frontend

--- build.js
+++ build.js
@@ -3,28 +3,17 @@ require('dotenv').config();
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const pEachSeries = require('p-each-series');
 const fs = require('fs-extra');
+const slugify = require('./lib/slugify');
 
 const doc = new GoogleSpreadsheet(
   '1oOCwb1pybm2RI6DSiBEv8W0xA06XPEoygVb6PnAuSno'
 );
 
 const links = {};
-
-function slugify(str) {
-  return str
-    .toString()
-    .toLowerCase()
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, ''); // Trim - from end of text
-}
+const componentCats = [];
 
 function isDriveLink(str) {
-  return !!(str && str.match(/^https:\/\/drive.google.com\/file/));
+  return !!(str && str.match(/^https:\/\/drive\.google\.com\/file\/d\/[^\/]+/));
 }
 
 async function auth() {
@@ -40,26 +29,90 @@ async function loadInfo() {
 
 async function processSheet(sheet) {
   const rows = await sheet.getRows();
+  // await sheet.loadCells();
+  const headers = sheet.headerValues;
   console.log('');
   console.log(`Processing sheet: ${sheet.title}`);
+  const components = [];
   rows.forEach((row) => {
+    // console.log(row.rowNumber);
     const id = row._rawData[0];
-    row._rawData.forEach((cell) => {
-      const driveLink = isDriveLink(cell);
-      if (driveLink) {
-        if (!id || !id.length) {
-          console.log(`no id for url ${cell}`);
-          process.exit(1);
+    const slug = slugify(id);
+    const filledCells = row._rawData.filter(
+      (cell) => cell && String(cell).length
+    );
+    let quantity = 0;
+    let datasheet = undefined;
+    let datasheetPreview = undefined;
+    if (filledCells.length) {
+      const fields = [];
+      for (let i = 1; i < headers.length; i++) {
+        const header = headers[i];
+        if (header && String(header).trim().length) {
+          const fieldVal = row._rawData[i];
+          if (fieldVal && String(fieldVal).trim().length) {
+            const headerNormalised = String(header).trim();
+            switch (headerNormalised.toLowerCase()) {
+              case 'datasheet':
+                datasheet = String(fieldVal).trim();
+                datasheetPreview = `https://drive.google.com/file/d/${
+                  datasheet.match(
+                    /^https:\/\/drive\.google\.com\/file\/d\/([^\/]+)/
+                  )[1]
+                }/preview`;
+                break;
+              case 'quantity':
+                quantity = Number(fieldVal);
+                break;
+              default:
+                const headerUnit = headerNormalised.match(
+                  /(.+)\s*\(\s*(.+)\s*\)$/
+                );
+                let processedFieldVal = String(fieldVal).trim();
+                if (processedFieldVal.match(/^\d+\.\d+$/)) {
+                  processedFieldVal = processedFieldVal
+                    .replace(/0+$/, '')
+                    .replace(/^(\d+)\.$/, '$1');
+                }
+                fields.push({
+                  label: headerUnit ? headerUnit[1].trim() : headerNormalised,
+                  value: headerUnit
+                    ? `${processedFieldVal}${headerUnit[2]}`
+                    : processedFieldVal,
+                });
+            }
+          }
         }
-        const slug = slugify(id);
-        if (links[slug]) {
-          console.log(`duplicate id: ${slug}`);
-          process.exit(1);
-        }
-        links[slug] = cell;
-        console.log(`${slug} - ${cell}`);
       }
-    });
+      components.push({
+        id,
+        slug,
+        quantity,
+        datasheet,
+        datasheetPreview,
+        fields,
+      });
+      row._rawData.forEach((cell) => {
+        const driveLink = isDriveLink(cell);
+        if (driveLink) {
+          if (!id || !id.length) {
+            console.log(`no id for url ${cell}`);
+            process.exit(1);
+          }
+          if (links[slug]) {
+            console.log(`duplicate id: ${slug}`);
+            process.exit(1);
+          }
+          links[slug] = cell;
+          console.log(`${slug} - ${cell}`);
+        }
+      });
+    }
+  });
+  componentCats.push({
+    title: sheet.title,
+    slug: slugify(sheet.title),
+    components,
   });
 }
 
@@ -71,4 +124,8 @@ function saveLinks() {
   return fs.outputJSON('./links.json', links, { spaces: 2 });
 }
 
-auth().then(loadInfo).then(processSheets).then(saveLinks);
+function saveInfo() {
+  return fs.outputJSON('./info.json', componentCats, { spaces: 2 });
+}
+
+auth().then(loadInfo).then(processSheets).then(saveLinks).then(saveInfo);
--- server.js
+++ server.js
@@ -1,20 +1,50 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
+const handlebars = require('express-handlebars');
 const links = require('./links.json');
+const info = require('./info.json');
 
 const PORT = process.env.PORT || 80;
 
+const app = express();
+
+app.use(express.static('public'));
+
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+function notFound(res, message = 'Not found.') {
+  return res.status(404).send(message);
+}
+
 app.get('/', (req, res) => {
-  res.send('OK');
+  res.render('home', { categories: info });
+});
+
+app.get('/c/:category', (req, res) => {
+  const category = info.find(
+    (category) => category.slug === req.params.category
+  );
+  if (!category) return notFound(res);
+  res.render('category', { category });
+});
+
+app.get('/c/:category/:component', (req, res) => {
+  const category = info.find(
+    (category) => category.slug === req.params.category
+  );
+  if (!category) return notFound(res);
+  const component = category.components.find(
+    (component) => component.slug === req.params.component
+  );
+  if (!component) return notFound(res);
+  res.render('component', { category, component });
 });
 
 app.get('/:slug', (req, res) => {
   const link = links[req.params.slug];
-  if (!link) {
-    return res.status(404).send('Not found.');
-  }
+  if (!link) return notFound(res);
   res.redirect(link);
 });
 
